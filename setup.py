000a98b72275ba86c1ec0df0ee8be8d5270cc816
playest <playest.ff@laposte.net>
2014-06-01 21:49:08 +0200

    add setup.py and remove pyxpath_old.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyxpath',
+      version='0.1',
+      description='PYthon XPATH interpretor',
+      author='playest',
+      author_email='playest.ff@laposte.net',
+     )
